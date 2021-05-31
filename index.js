5dfc0c9a05599f0460f90a07240b4b3811337547
Glenn Goossens <glgoose@pm.me>
2021-05-31 20:02:11 +0200

    rename functions

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const sun = document.getElementById('sun')
 const pig = document.getElementById('pig')
 const question = document.querySelector('.question')
 
-const transform = (el, factorX = 0, factorY = 0) => {
+const moveAll = (el, factorX = 0, factorY = 0) => {
   const { scrollY } = window
 
   el.style.transform = `translateX(${scrollY *
     factorX}px) translateY(${scrollY * factorY}px)`
 }
 
-function move () {
-  transform(sun, -0.2, 0.5)
-  transform(pig, 0.7, 0.2)
-  transform(question, 0, 0.9)
+function moveAll () {
+  move(sun, -0.2, 0.5)
+  move(pig, 0.7, 0.2)
+  move(question, 0, 0.9)
 }
 
 window.addEventListener('scroll', move)
