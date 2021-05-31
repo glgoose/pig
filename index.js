0f877087f2cf0241abc6bd5dafea51604fe86b20
Glenn Goossens <glgoose@pm.me>
2021-05-31 19:59:59 +0200

    refactor index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const sky = document.getElementById('sky')
 const sun = document.getElementById('sun')
 const pig = document.getElementById('pig')
-const field = document.getElementById('field')
 const question = document.querySelector('.question')
 
-const transform = (el, scrollY, factorX = 0, factorY = 0) =>
-  (el.style.transform = `translateX(${scrollY *
-    factorX}px) translateY(${scrollY * factorY}px)`)
+const transform = (el, factorX = 0, factorY = 0) => {
+  const { scrollY } = window
+
+  el.style.transform = `translateX(${scrollY *
+    factorX}px) translateY(${scrollY * factorY}px)`
+}
 
 function move () {
-  const { scrollY, innerHeight: windowHeight } = window
-
-  transform(sun, scrollY, -0.2, 0.5)
-  transform(pig, scrollY, 0.7, 0.2)
-  transform(question, scrollY, 0, 0.9)
+  transform(sun, -0.2, 0.5)
+  transform(pig, 0.7, 0.2)
+  transform(question, 0, 0.9)
 }
 
 window.addEventListener('scroll', move)
